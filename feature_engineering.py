42ac48fd49ff8f7fcbd2b4c310902828a7e9080b
Michael Rowland <mrowland13@gmail.com>
2021-02-17 11:15:44 -0600

    added functionality for generating a tidy leverage index lookup table

--- feature_engineering.py
+++ feature_engineering.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 DATA_FILE = './combined.csv'
 
-def read_data(filepath: str):
+def read_data(filepath: str) -> pd.DataFrame:
     """
     Read a single ``csv`` file into a single DataFrame
     Cleans data, dropping duplicates, drop pitches that do not result in event
@@ -26,6 +26,40 @@ def read_data(filepath: str):
     df = df.dropna(subset=['events'])
     return df
 
+
+def generate_lookup(path: str = None) -> pd.DataFrame:
+
+    if not path:
+        path = './tidy_lookup.csv'
+
+    df = pd.read_csv('https://raw.githubusercontent.com/michael-rowland/stressful-baseball/main/li_lookup.csv')
+
+    # Converts bases to boolean on seperate columns
+    # https://chrisalbon.com/python/data_wrangling/pandas_expand_cells_containing_lists/
+    bases = (
+        df['bases']
+        .apply(lambda x: [True if base.isnumeric() else False for base in x.split(' ')])
+        .apply(pd.Series)
+        .rename(columns = lambda x: f'on_{x+1}b')
+    )
+    df = pd.concat([df[:], bases[:]], axis=1)
+
+    # make data "tidy"
+    df = df.melt(
+        id_vars=['inning', 'inning_topbot', 'outs', 'on_1b', 'on_2b', 'on_3b'],
+        value_vars=['-4', '-3', '-2', '-1', '0', '1', '2', '3', '4'],
+        var_name='score_diff',
+        value_name='leverage'
+    )
+    df['score_diff'] = df['score_diff'].astype(int)
+    df = df.dropna(subset=['leverage'])
+
+    df.to_csv(path_or_buf=path, index=False)
+    return df
+
 if __name__ == "__main__":
     df = read_data(DATA_FILE)
-    print(df.head())
+    print(df.shape)
+
+    lookup = generate_lookup()
+    print(lookup.shape)
